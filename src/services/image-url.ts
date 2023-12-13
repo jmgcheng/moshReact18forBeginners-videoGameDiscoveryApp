b6fdcce0f4113aa426be8ff868b5c2b8e815c8a3
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-13 13:27:08 +0800

    24-sortingGames

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,6 @@
 const getCroppedImageUrl = (url: string) => {
+    if(!url) return '';
+
     const targert = 'media/'
     const index = url.indexOf(targert) + targert.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
