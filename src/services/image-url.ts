05084fdb3fdc281e711ef5cfb2eeafb737a47532
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-13 13:32:23 +0800

    25-handlingGamesWithoutImage

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImageUrl = (url: string) => {
-    if(!url) return '';
+    if(!url) return noImage;
 
     const targert = 'media/'
     const index = url.indexOf(targert) + targert.length;
