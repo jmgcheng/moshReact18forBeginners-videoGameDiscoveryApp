09fcdd177f957be56f2e86b72a39f85a43a764c7
jmgcheng <jmgcheng@users.noreply.github.com>
2023-12-13 08:25:09 +0800

    10-displayingCriticScore

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,11 +8,13 @@ export interface Platform {
     slug: string;
 }
 
+// check developer console > Network > Fetch/XHR > games request > Preview > result object for fields you can put here
 export interface Game {
     id: number;
     name: string;
     background_image: string;
     parent_platforms: { platform: Platform }[]
+    metacritic: number;
 }
 
 interface FetchGamesResponse {
